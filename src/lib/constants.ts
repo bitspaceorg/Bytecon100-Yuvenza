d6a55cdfbbda896bd1285df44daf51c5a2022e1c
Rahul M. Navneeth <50259284+RahulNavneeth@users.noreply.github.com>
2023-08-10 21:12:00 +0530

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 const TEXT_FILE_NAME = "PunIntended_PullRequest";
-export const FETCH_URL = `https://github.com/bitspaceorg/yuvenza-workshop/blob/master/${TEXT_FILE_NAME}.txt`;
+export const FETCH_URL = `https://github.com/bitspaceorg/Bytecon100-Yuvenza/blob/master/${TEXT_FILE_NAME}.txt`;
 
 export const HASH_ENDPOINT = "https://jwthash.onrender.com/";
 
